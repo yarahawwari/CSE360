dde99a32b1ca1209508e793eed190acb82cd72a9
yarahawwari <yarahawwari@192.168.0.9>
2019-02-25 19:20:29 -0700

    Initial commit of project

---
+++ CSE360/src/CSE360Assign2/Calculator.java
@@ -0,0 +1,39 @@
+/*Name: Yara Hawwari
+ * Class ID: 400
+ * Assignment 3
+ * Description: 
+ */
+package CSE360Assign2;
+
+public class Calculator {
+
+	private int total;
+	
+	public Calculator () {
+		total = 0;  // not needed - included for clarity
+	}
+	
+	public int getTotal () {
+		return 0;
+	}
+	
+	public void add (int value) {
+		
+	}
+	
+	public void subtract (int value) {
+		
+	}
+	
+	public void multiply (int value) {
+		
+	}
+	
+	public void divide (int value) {
+		
+	}
+	
+	public String getHistory () {
+		return "";
+	}
+}
